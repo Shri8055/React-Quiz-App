94df54a29d7341274fa6cf291a927b8043d79a04
Shri8055 <shrinivaskangralkar8055@gmail.com>
2024-12-10 12:09:19 +0530

    first commit

--- src/ui/Main.jsx
+++ src/ui/Main.jsx
@@ -10,7 +10,7 @@ function Main(){
         <div className='main-container' >
             <p>Instructions :</p><hr/>
             <li>This is a FREE online test. Beware of scammers who ask for money to attend this test.</li>
-            <li>Total number of questions: 20.</li>
+            <li>Total number of questions: 10.</li>
             <li>Time allotted: 30 minutes.</li>
             <li>Each question carries 1 mark; there are no negative marks.</li>
             <li>All the best!</li>
