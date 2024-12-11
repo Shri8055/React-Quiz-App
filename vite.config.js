35f4e53f6bf2d6c820d814f617322dbba9718dd1
Shrinivas Mahendra Kangralkar <88592860+Shri8055@users.noreply.github.com>
2024-12-11 19:53:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/React-Quiz-App/', // This should match your repository name
+  base: '/React-Quiz-App/', // Update to match your GitHub repository name
 });
